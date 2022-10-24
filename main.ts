f4745049d33db925f96d408fb005129b90ce8690
AJSchroeder83 <116520195+AJSchroeder83@users.noreply.github.com>
2022-10-24 14:10:14 +0200

    Werte Gerundet, Ausgabe über Seriell verbessert

--- main.ts
+++ main.ts
@@ -1,15 +1,16 @@
-input.onButtonPressed(Button.A, function () {
-	
-})
 let LichtWest = 0
 let LichtOst = 0
-let Position = 180
+let Position = 90
 basic.forever(function () {
-    LichtOst = Math.map(pins.analogReadPin(AnalogPin.P0), 0, 1023, 0, 100)
-    LichtWest = Math.map(pins.analogReadPin(AnalogPin.P1), 0, 1023, 0, 100)
+    LichtOst = Math.ceil(Math.map(pins.analogReadPin(AnalogPin.P0), 0, 1023, 0, 100))
+    LichtWest = Math.ceil(Math.map(pins.analogReadPin(AnalogPin.P1), 0, 1023, 0, 100))
+    serial.writeValue("Licht Ost", LichtOst)
+    serial.writeValue("Licht West", LichtWest)
+    basic.pause(100)
     if (LichtOst > LichtWest) {
         if (Position > 0) {
             Position = Position - 1
+            serial.writeValue("Position", Position)
         }
         basic.showLeds(`
             . . # . .
@@ -18,10 +19,12 @@ basic.forever(function () {
             . # . . .
             . . # . .
             `)
+        pins.servoWritePin(AnalogPin.P5, Position)
     }
     if (LichtOst < LichtWest) {
         if (Position < 180) {
             Position = Position + 1
+            serial.writeValue("Position", Position)
         }
         basic.showLeds(`
             . . # . .
@@ -30,9 +33,10 @@ basic.forever(function () {
             . . . # .
             . . # . .
             `)
+        pins.servoWritePin(AnalogPin.P5, Position)
     }
     if (LichtOst == LichtWest) {
         basic.showIcon(IconNames.Happy)
+        serial.writeValue("Position erreicht", Position)
     }
-    pins.servoWritePin(AnalogPin.P5, Position)
 })
